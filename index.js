7094006145ab31b77bc7925557e45c118f627392
Asd396 <leokotipalo@gmail.com>
2020-02-09 16:46:03 +0200

    3.5

--- index.js
+++ index.js
@@ -50,6 +50,41 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+const generateId = () => {
+  return Math.round(Math.random() * Number.MAX_SAFE_INTEGER)
+}
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(note => note.id === id)
+  if (person) {
+    persons = persons.filter(person => person.id !== id)
+    response.status(204)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
