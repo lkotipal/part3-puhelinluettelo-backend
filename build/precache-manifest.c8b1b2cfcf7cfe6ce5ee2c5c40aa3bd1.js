f9fe5fd4349ba519834919293d48812fe5c61f47
Asd396 <leokotipalo@gmail.com>
2020-02-10 20:24:46 +0200

    uibuild

---
+++ build/precache-manifest.c8b1b2cfcf7cfe6ce5ee2c5c40aa3bd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0e83fb8ed6bb048b2efe57c16d50c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84a671a5f18b4c9bbd2f",
+    "url": "/static/css/main.efc48fce.chunk.css"
+  },
+  {
+    "revision": "8514db5cee128984cd03",
+    "url": "/static/js/2.16d1eaee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.16d1eaee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a671a5f18b4c9bbd2f",
+    "url": "/static/js/main.3ee40412.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
